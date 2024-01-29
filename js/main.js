014b0022303d832c09568e969bb656f502943e54
mielecarmine <mielecarmine993@gmail.com>
2024-01-29 15:57:58 +0100

    feat: print Hello vue and img

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const app = createApp({
   data() {
     return {
       message: "Hello Vue!",
+      image:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/248_f2.png",
     };
   },
 }).mount("#app");
